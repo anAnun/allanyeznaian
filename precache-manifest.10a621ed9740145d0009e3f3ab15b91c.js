484334446a463109e642fe9e99dbe43670bf8697
Allan Yeznaian <allanyeznaian@gmail.com>
2018-11-21 17:59:00 -0800

    Updates

---
+++ precache-manifest.10a621ed9740145d0009e3f3ab15b91c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/allanyeznaian/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0a4e9db3152831ba3906",
+    "url": "/allanyeznaian/static/js/runtime~main.0a4e9db3.js"
+  },
+  {
+    "revision": "6740d543af83a7a83836",
+    "url": "/allanyeznaian/static/js/main.6740d543.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/allanyeznaian/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "6740d543af83a7a83836",
+    "url": "/allanyeznaian/static/css/main.892d1b7d.chunk.css"
+  },
+  {
+    "revision": "d3c30ca66a4662e55538a834986a8acf",
+    "url": "/allanyeznaian/index.html"
+  }
+];
